1014869e95a018b2b07f154c743d21b878bd76cb
qly <qly@100861131>
2024-12-25 17:12:44 +0800

    提交代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <h1>欢聚一堂222</h1>
+  </div>
 </template>
